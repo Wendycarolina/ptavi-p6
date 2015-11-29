bf6a17da47bde68655215c6229e7e64d75aeb606
wendycarolina <carolina.ia.g@gmail.com>
2015-11-29 11:15:48 +0100

    comprueba audio_file

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
 import os
 
 
@@ -27,9 +28,16 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 
 if __name__ == "__main__":
     #Argumentos del servidor
-    IP = sys.argv[1]
-    PORT = int(sys.argv[2])
-    AUDIO = sys.argv[3]
+    try:
+        IP = sys.argv[1]
+        PORT = int(sys.argv[2])
+        AUDIO = sys.argv[3]
+        if len(sys.argv) != 4 or not os.path.exists(AUDIO):
+            print('Usage: python server.py IP port audio_file')
+            raise SystemExit
+    except IndexError:
+        sys.exit('Usage: python server.py IP port audio_file')
+
     serv = socketserver.UDPServer(('', PORT), EchoHandler)
     print("Listening...")
     serv.serve_forever()
