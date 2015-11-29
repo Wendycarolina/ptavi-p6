c39a1b0dcd758cdc9e47c7fc697bef281b95fa0f
wendycarolina <carolina.ia.g@gmail.com>
2015-11-29 12:32:39 +0100

    INVITE

--- server.py
+++ server.py
@@ -13,15 +13,24 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion")
+        self.wfile.write(b"Hemos recibido tu peticion" + b'\r\n\r\n' )
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
-
+            linea = line.decode('utf-8')
+            print("El cliente nos manda " + linea)
+            linea = linea.split()
+            Metodo = linea[0]
+            print(Metodo)
+            if Metodo == 'INVITE':
+                envio = b'SIP/2.0 ' + b'100 TRYING ' + b'\r\n\r\n'
+                envio += b'SIP/2.0 ' + b'180 RINGING ' + b'\r\n\r\n'
+                envio += b'SIP/2.0 ' + b'200 OK ' + b'\r\n\r\n'
+                self.wfile.write(envio)
+                print(envio)
+            
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
